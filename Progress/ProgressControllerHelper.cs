e5cb988bc3e5e5e72c6052673c85bdbb9a3da60c
Evgeniy Plyusnin <eninbox@gmail.com>
2017-05-10 14:57:08 +0500

    Немного помощников

---
+++ Progress/ProgressControllerHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace AsyncOperations.Progress
+{
+    public static class ProgressControllerHelper
+    {
+        /// <summary>Устанавливает Intermediate-значение прогресса операции с описанием</summary>
+        /// <param name="Controller">Контроллер прогресса</param>
+        /// <param name="MessageFormat">Формат сообщения</param>
+        public static void SetToIntermediate(this IProgressController Controller, string MessageFormat)
+        {
+            Controller.SetToIntermediate();
+            Controller.SetDescription(MessageFormat);
+        }
+    }
+}
