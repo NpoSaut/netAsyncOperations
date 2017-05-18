50e44af80af138aee265f22a4e1de93778188863
Evgeniy Plyusnin <eninbox@gmail.com>
2017-05-18 17:52:25 +0500

    Метод-расширение для IProgressToken для получения IProgressController по-умолчанию

--- Progress/ProgressControllerFactory.cs
+++ Progress/ProgressControllerFactory.cs
@@ -2,6 +2,10 @@
 {
     public class ProgressControllerFactory : IProgressControllerFactory
     {
+        static ProgressControllerFactory() { Default = new ProgressControllerFactory(); }
+
+        public static IProgressControllerFactory Default { get; }
+
         public IProgressController CreateController(IProgressToken Token) { return new ProgressController(Token); }
     }
 }
--- Progress/ProgressControllerHelper.cs
+++ Progress/ProgressControllerHelper.cs
@@ -2,6 +2,12 @@
 {
     public static class ProgressControllerHelper
     {
+        /// <summary>Создаёт экземпляр <see cref="IProgressController" /> по-умолчанию</summary>
+        public static IProgressController Controller(this IProgressToken ProgressToken)
+        {
+            return ProgressControllerFactory.Default.CreateController(ProgressToken);
+        }
+
         /// <summary>Устанавливает Intermediate-значение прогресса операции с описанием</summary>
         /// <param name="Controller">Контроллер прогресса</param>
         /// <param name="MessageFormat">Формат сообщения</param>
